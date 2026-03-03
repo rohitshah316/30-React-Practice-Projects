abe795dcfe7094d4601e9c61466802b338185388
Rohit Shah <shahrohit2223@gmail.com>
2026-03-03 12:36:38 +0545

    16 LocalStorage Sync App(Counter App)

---
+++ 16 LocalStorage Sync App/src/components/Counter.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState } from 'react'
+import './Counter.css'
+import { useEffect } from 'react'
+
+const Counter = () => {
+
+    const useLocalStorage=(key,intialValue)=>{
+        const [value,setValue]=useState(()=>{
+            const saved=localStorage.getItem(key);
+            return saved!==null?JSON.parse(saved):intialValue;
+        })
+
+        useEffect(()=>{
+            localStorage.setItem(key,JSON.stringify(value))
+        },[key,value])
+
+
+        return [value,setValue]
+    }
+
+    const [count,setCount]=useLocalStorage("count",0)
+
+  return (
+    <>
+    <h1>Counter App Sync With LocalStorage
+    </h1>
+
+    <h2>Count: {count}</h2>
+    <button onClick={()=>setCount(c=>c+1)}>Count</button>
+    <button onClick={()=>setCount(0)}>Reset</button>
+    </>
+  )
+}
+
+export default Counter
